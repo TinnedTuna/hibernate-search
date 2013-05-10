f28ceed5aa66e7272a69b90e006026e541c5c9c2
Dan Turner <dst502@york.ac.uk>
2013-05-10 10:03:57 +0100

    AbstractDAO's checkstyle.

--- integrationtest/src/test/java/org/hibernate/search/test/integration/jtaspring/sessionfactory/AbstractDAO.java
+++ integrationtest/src/test/java/org/hibernate/search/test/integration/jtaspring/sessionfactory/AbstractDAO.java
@@ -1,3 +1,23 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
 package org.hibernate.search.test.integration.jtaspring.sessionfactory;
 
 import java.io.Serializable;
@@ -6,8 +26,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,30 +33,30 @@ public abstract class AbstractDAO<T, PK extends Serializable> {
 	private SessionFactory sessionFactory;
         Class<T> type;
 	
- 	protected AbstractDAO() {
+  protected AbstractDAO() {
 		// Null constructor for CGLIB
-	}
+  }
 
 	public AbstractDAO(Class<T> type,
 		 SessionFactory sessionFactory) {
 		this.type = type;
-		this.sessionFactory = sessionFactory;
+    this.sessionFactory = sessionFactory;
 	}
 
 	public PK save(T obj) {
 		return (PK) getSession().save(obj);
 	}
 
-        public T read(PK id) {
+  public T read(PK id) {
 		T result = (T) getSession().load(type, id);
-		return result;
+    return result;
 	}
 	
 	public Transaction beginTransaction() {
 		return getSession().beginTransaction();
 	}
 
-        protected Session getSession() {
+  protected Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
 	
@@ -47,6 +65,6 @@ public abstract class AbstractDAO<T, PK extends Serializable> {
 	}
 
 	public void setType(Class<T> type) {
-		this.type=type;
+		this.type = type;
 	}
 }
